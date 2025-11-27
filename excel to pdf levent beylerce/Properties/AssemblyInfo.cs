edf770c0861a73ee45609488938fda8365a81e48
x <x@DESKTOP-U9D60O2>
2025-11-27 12:35:51 +0300

    Versiyon 3 Yayımlandı.

--- excel to pdf levent beylerce/Properties/AssemblyInfo.cs
+++ excel to pdf levent beylerce/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77ebe092-b2d0-4e15-b644-ae5d2ff6dcaf")]
+[assembly: Guid("77ebe092-b2d0-4e15-b644-ae5d2ff6dca2")]
 
 // Version information for an assembly consists of the following four values:
 //
